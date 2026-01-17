d90a27e6a211932dae12d76864bc89d42ad0c564
rohit3607 <rohitplayer87089@gmail.com>
2026-01-17 14:07:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config(object):
     # database config
     DB_NAME = os.environ.get("DB_NAME","Yato")     
     DB_URL  = os.environ.get("DB_URL","mongodb+srv://rohitplayer87089:rohit870@cluster0.4wt927p.mongodb.net/?retryWrites=true&w=majority")
-    PORT = os.environ.get("PORT", "8020")
+    PORT = os.environ.get("PORT", "8077")
  
     # other configs
     BOT_UPTIME  = time.time()
